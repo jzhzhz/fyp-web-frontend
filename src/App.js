90a53af72ef06777bfb7f201322d095c43778a24
hzzhang <monsttrod@gmail.com>
2020-04-19 13:23:51 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import { People } from './People';
 import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
 import NavigationBar from './components/NavigationBar';
+import HomeFooter from './components/HomeFooter';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Router>
-          <NavigationBar />         
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/people" component={People} />
-              <Route component={NoMatch} />
-            </Switch>
+          <NavigationBar />
+          <div style={{minHeight: "80vh"}}>      
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/people" component={People} />
+            <Route component={NoMatch} />
+          </Switch>
+          </div>   
+          <HomeFooter />
         </Router>
       </React.Fragment>
     );
