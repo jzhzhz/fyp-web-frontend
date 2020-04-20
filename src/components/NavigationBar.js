1dcefe9e16b7e8d668182bf24abee9c853cf5034
hzzhang <monsttrod@gmail.com>
2020-04-20 15:22:34 +0800

    update navbar, include dynamic labels

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown, Container} from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 import styled from 'styled-components';
 import axios from 'axios';
 
@@ -8,25 +8,91 @@ const Styles = styled.div`
 
 class NavigationBar extends React.Component {
   /* TODO */
-  // About, Academic, Admissions should be dynamic
   // staff page inside people nav should be dynamic
   constructor() {
     super();
+    this.state = {
+      aboutLabels: [],
+      academicsLabels: [],
+      admissionsLabels: []
+    };
   }
 
-  handleClick = () => {
-    axios.get("http://localhost:10480/csWeb/hello")
-      .then((res) => {
-        console.log(res);
-        // alert(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .then(() => {
-        console.log("finish requesting backend")
+  getAboutLabel = async () => {
+    const res = await axios.get("http://localhost:10480/csWeb/getAboutLabels");
+
+    if (res.status === 200 && res.data.code === 0) {
+      let aboutLabelsRes = res.data.data;
+
+      aboutLabelsRes = aboutLabelsRes.map(
+        obj => <NavDropdown.Item key={obj.id} href="#">{obj.label}</NavDropdown.Item>
+      );
+
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          aboutLabels: aboutLabelsRes
+        };
       });
+      console.log("get About labels success!");
+    } else {
+      console.log("get About labels failed!");
+    }
   }
+
+  getAcademicsLabel = async () => {
+    const res = await axios.get("http://localhost:10480/csWeb/getAcademicsLabels");
+
+    if (res.status === 200 && res.data.code === 0) {
+      let academicsLabelsRes = res.data.data;
+
+      academicsLabelsRes = academicsLabelsRes.map(
+        obj => <NavDropdown.Item key={obj.id} href="#">{obj.label}</NavDropdown.Item>
+      );
+
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          academicsLabels: academicsLabelsRes
+        };
+      });
+      console.log("get Academics labels success!");
+    } else {
+      console.log("get Academics labels failed!");
+    }
+  }
+
+  getAdmissionsLabel = async () => {
+    const res = await axios.get("http://localhost:10480/csWeb/getAdmissionsLabels");
+
+    if (res.status === 200 && res.data.code === 0) {
+      let admissionsLabelsRes = res.data.data;
+
+      admissionsLabelsRes = admissionsLabelsRes.map(
+        obj => <NavDropdown.Item key={obj.id} href="#">{obj.label}</NavDropdown.Item>
+      );
+
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          admissionsLabels: admissionsLabelsRes
+        };
+      });
+      console.log("get Admissions labels success!");
+    } else {
+      console.log("get Admissions labels failed!");
+    }
+  }
+
+  componentWillMount() {
+    this.getAboutLabel();
+    this.getAcademicsLabel();
+    this.getAdmissionsLabel();
+  }
+
+  // handleClick = () => {
+
+  // }
 
   render() {
     return (
@@ -38,22 +104,20 @@ class NavigationBar extends React.Component {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
                 <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                <NavDropdown title="About" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#" onClick={this.handleClick}>Action</NavDropdown.Item>
-                  <NavDropdown.Item href="/">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="/">Something</NavDropdown.Item>
+                <NavDropdown title="About" id="basic-nav-dropdown" onClick={this.handleClick}>
+                  {this.state.aboutLabels}
                 </NavDropdown>
                 <NavDropdown title="People" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="/people">Faculty</NavDropdown.Item>
-                  <NavDropdown.Item href="/">Research Staff</NavDropdown.Item>
-                  <NavDropdown.Item href="/">Administrative Staff</NavDropdown.Item>
+                  <NavDropdown.Item href="/faculty">Faculty</NavDropdown.Item>
+                  <NavDropdown.Item href="/researchStaff">Research Staff</NavDropdown.Item>
+                  <NavDropdown.Item href="/adminStaff">Administrative Staff</NavDropdown.Item>
                 </NavDropdown>
                 <Nav.Item><Nav.Link href="/research">Research</Nav.Link></Nav.Item>
                 <NavDropdown title="Academics" id="basic-nav-dropdown">
-                  {/* TODO */}
+                  {this.state.academicsLabels}
                 </NavDropdown>
                 <NavDropdown title="Admissions" id="basic-nav-dropdown">
-                  {/* TODO */}
+                  {this.state.admissionsLabels}
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
