5a4dfe359a5dd816da8dc442734a5e8ad5d4d09c
hzzhang <monsttrod@gmail.com>
2020-04-23 15:25:54 +0800

    adjust home page layout

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ export const Home = () => (
     <Jumbotron />
     <Container>
       <Row>
-        <Col sm={10}>
+        <Col sm={9}>
           <h2>Headline Information</h2>
           <hr />
           <HomeCarousel />
@@ -20,7 +20,7 @@ export const Home = () => (
           <p>Indulgence announcing uncommonly met she continuing two unpleasing terminated. Now busy say down the shed eyes roof paid her. Of shameless collected suspicion existence in. Share walls stuff think but the arise guest. Course suffer to do he sussex it window advice. Yet matter enable misery end extent common men should. Her indulgence but assistance favourable cultivated everything collecting. </p>
           <hr />
         </Col>
-        <Col sm={2}>
+        <Col sm={3}>
           <h5>side line information</h5>
           <p>Or kind rest bred with am shed then. In raptures building an bringing be.</p>
           <p>Better of always missed we person mr. September smallness northward situation few her certainty something. </p>
