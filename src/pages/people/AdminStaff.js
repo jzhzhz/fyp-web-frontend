b2f1b009cd00645e1852167781e9b870ace3a6cc
hzzhang <monsttrod@gmail.com>
2020-05-19 21:24:22 +0800

    make people pages retrieve data from backend

--- src/pages/people/AdminStaff.js
+++ src/pages/people/AdminStaff.js
@@ -1,7 +1,77 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
+import axios from 'axios';
+import _ from 'lodash';
 
 class AdminStaff extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      facultyList: [],
+      facultyListElement: []
+    };
+  }
+
+  componentDidMount() {
+    this.getFacultyByType("admin");
+  }
+
+  getFacultyByType = async (type) => {
+    let res = {};
+    const url = process.env.REACT_APP_BACKEND_URL + "/getFacultyByType?type=" + type;
+    
+    await axios.get(url)
+      .then((getRes) => {
+        res = getRes;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    if (res.status === 200 && res.data.code === 0) {
+      this.setState({
+        facultyList: res.data.data
+      }, () => {
+        this.renderFacultyListElement(this.state.facultyList);
+      });
+    }
+  }
+
+  renderFacultyListElement = (facultyList) => {
+    facultyList = facultyList.map((faculty, index) => {
+      let link;
+      if (faculty.url === "") {
+        link = faculty.name;
+      } else if (faculty.url.includes("https://")) {
+        link = <a 
+          target="_blank" 
+          href={faculty.url} 
+          rel="noopener noreferrer"
+          style={{color: "black"}}
+        >
+          {faculty.name}
+        </a>;
+      } else {
+        link = <a href={faculty.url} style={{color: "black"}}>{faculty.name}</a>;
+      }
+
+      return (
+        <React.Fragment>
+          <tr>
+            <td>{link}</td>
+            <td>{faculty.phone}</td>
+            <td>{faculty.office}</td>
+            <td>{faculty.email}</td>
+          </tr>
+        </React.Fragment>
+      );
+    });
+
+    this.setState({
+      facultyListElement: _.cloneDeep(facultyList)
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -10,8 +80,8 @@ class AdminStaff extends React.Component {
         <hr />
         <p>Administration</p>
         
-        <Table striped bordered hover size="sm">
-          <thead>
+        <Table striped bordered hover size="sm" style={{textAlign: "center"}}>
+          <thead style={{backgroundColor: "#066baf", color: "white"}}>
             <tr>
               <th>name</th>
               <th>phone</th>
@@ -20,30 +90,7 @@ class AdminStaff extends React.Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Mark Otto</td>
-              <td>123</td>
-              <td>102</td>
-              <td>123@abc</td>
-            </tr>
-            <tr>
-              <td>Jacob Thorton</td>
-              <td>123</td>
-              <td>102</td>
-              <td>123@abc</td>
-            </tr>
-            <tr>
-              <td>Larry Bird</td>
-              <td>123</td>
-              <td>102</td>
-              <td>123@abc</td>
-            </tr>
-            <tr>
-              <td>Jessica Jones</td>
-              <td>123</td>
-              <td>102</td>
-              <td>123@abc</td>
-            </tr>
+            {this.state.facultyListElement}
           </tbody>
         </Table>
       </React.Fragment>
