806d0d80ba27850f26158b42547aea7e6155050b
hzzhang <monsttrod@gmail.com>
2020-05-31 22:20:22 +0800

    add another download link

--- src/pages/admin/AdminBackendManage.js
+++ src/pages/admin/AdminBackendManage.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Table, Form, Spinner } from 'react-bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  a {
+    color: #6097c4;
+  }
+`;
 
 class AdminBackendManage extends React.Component {
   constructor() {
@@ -53,79 +60,102 @@ class AdminBackendManage extends React.Component {
     const spin = this.state.waiting ? <Spinner animation="border" size="sm" /> : null;
 
     return (
-      <React.Fragment>
-        <h3>Backend Data Download & Update</h3>
-        <a style={{color: "gray"}} href="/admin/main">{"<<"} return to main setting page</a>
-        <hr />
-        <h5>Download Backend Files</h5>
+      <Styles>
+        <React.Fragment>
+          <h3>Backend Data Download & Update</h3>
+          <a style={{color: "gray"}} href="/admin/main">{"<<"} return to main setting page</a>
+          <hr />
+          <h5>Download Backend Files</h5>
 
-        <Table striped bordered hover size="sm" style={{marginTop: "20px", width: "800px"}}>
-          <thead style={{backgroundColor: "#066baf", color: "white"}}>
-            <tr>
-              <th>file</th>
-              <th>description</th>
-              <th>last update</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>            
-                <a 
-                  href={process.env.REACT_APP_ADMIN_URL + "/getFacultyExcelFile"}
-                  style={{color: "black", marginRight: "10px"}}
-                >
-                  [faculty-list.xlsx]
-                </a>
-              </td>
-              <td>contains list of faculty and admin information</td>
-              <td>2020.5.30</td>
-            </tr>
+          <Table striped bordered hover size="sm" style={{marginTop: "20px", width: "800px"}}>
+            <thead style={{backgroundColor: "#066baf", color: "white"}}>
+              <tr>
+                <th>file</th>
+                <th>description</th>
+                <th>last update</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>            
+                  <a 
+                    href={process.env.REACT_APP_ADMIN_URL + "/getFacultyExcelFile"}
+                    style={{marginRight: "10px"}}
+                  >
+                    [faculty-list.xlsx]
+                  </a>
+                </td>
+                <td>contains lists of faculty and admin information</td>
+                <td>2020.5.30</td>
+              </tr>
 
-            <tr>
-              <td>
-                <a 
-                  href={process.env.REACT_APP_ADMIN_URL + "/getBackendData"}
-                  style={{color: "black"}}
-                >
-                  [backend-data.zip]
-                </a>
-              </td>
-              <td>
-                zip file for all backend data, including code segment
-              </td>
-              <td>2020.5.30</td>
-            </tr>
-          </tbody>
-        </Table>
-        <br />
+              <tr>
+                <td>            
+                  <a 
+                    href={process.env.REACT_APP_ADMIN_URL + "/getHomePageExcelFile"}
+                    style={{marginRight: "10px"}}
+                  >
+                    [home-information.xlsx]
+                  </a>
+                </td>
+                <td>contains lists of home information</td>
+                <td>2020.5.31</td>
+              </tr>
 
-        <h5>Upload Excel File to Update Backend Information</h5>
-        <Form>
-          <Form.Group>
-            <Form.Label>The file format should be EXACTLY the same as the file above!</Form.Label>
-            <br />
-            <Form.Label>File name will be recognized to update corresponding original data.</Form.Label>
-            <br />
-            <Form.File label="Custom file input" style={{width: "400px"}} custom> 
-            <Form.File.Input 
-              isValid={this.state.isExcelValid}
-              isInvalid={!this.state.isExcelValid} 
-              onChange={this.handleExcelFileChange}
-            />
-            <Form.File.Label data-browse="Choose File">
-              upload excel file here
-            </Form.File.Label>
-            {spin}
-            <Form.Control.Feedback type="valid">
-              excel file upload success, data updated success!
-            </Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid" style={{color: "red"}}>
-              invalid file!
-            </Form.Control.Feedback>
-          </Form.File>
-          </Form.Group>
-        </Form>
-      </React.Fragment>
+              <tr>
+                <td>
+                  <a 
+                    href={process.env.REACT_APP_ADMIN_URL + "/getBackendData"}
+                  >
+                    [backend-data.zip]
+                  </a>
+                </td>
+                <td>
+                  zip file for all backend data, including code segment
+                </td>
+                <td>2020.5.30</td>
+              </tr>
+            </tbody>
+          </Table>
+          <br />
+
+          <h5>Upload Excel File to Update Faculty Information</h5>
+          <Form style={{marginTop: "15px"}}>
+            <Form.Group>
+              <Form.Label>
+                The file format should be EXACTLY the same as the  
+                  <a 
+                    href={process.env.REACT_APP_ADMIN_URL + "/getFacultyExcelFile"}
+                    style={{marginRight: "8px", marginLeft: "8px"}}
+                  >
+                    [faculty-list.xlsx]
+                  </a> 
+                above!
+              </Form.Label>
+              <br />
+              <Form.Label>File name will be recognized to update corresponding original data.</Form.Label>
+              <br />
+              <Form.File label="Custom file input" style={{width: "400px"}} custom> 
+              <Form.File.Input 
+                isValid={this.state.isExcelValid}
+                isInvalid={!this.state.isExcelValid} 
+                onChange={this.handleExcelFileChange}
+              />
+              <Form.File.Label data-browse="Choose File">
+                upload excel file here
+              </Form.File.Label>
+              {spin}
+              <Form.Control.Feedback type="valid">
+                excel file upload success, data updated success!
+              </Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid" style={{color: "red"}}>
+                invalid file!
+              </Form.Control.Feedback>
+            </Form.File>
+            </Form.Group>
+          </Form>
+        </React.Fragment>
+      </Styles>
     );
   }
 }
