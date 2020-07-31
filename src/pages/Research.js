7f1959b767b58324403e80286a0a1935cb1e5740
hzzhang <monsttrod@gmail.com>
2020-07-31 14:11:53 +0800

    put in dummy words in static page

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -4,7 +4,27 @@ export const Research = () => (
   <React.Fragment>
     <h2>Research</h2>
     <hr />
-    <p>Note that the development build is not optimized.</p>
-    <p>To create a production build, use yarn build. </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt a ipsum at iaculis. 
+      Nam convallis erat ut ex placerat condimentum. Nulla sed auctor urna. Nunc blandit orci et posuere consequat. 
+      Donec elementum, leo eu feugiat commodo, odio odio pretium libero, sagittis egestas justo lorem eu mauris. 
+      Fusce vulputate massa nec enim pulvinar, in condimentum neque finibus. 
+      Phasellus nisi quam, ultrices a scelerisque sed, commodo nec ligula. 
+      Aliquam erat volutpat. Curabitur consectetur nibh non orci dignissim, quis consectetur arcu faucibus. 
+      Cras varius vitae turpis vel pellentesque. Phasellus vulputate libero magna, id venenatis mauris pellentesque nec. 
+      Suspendisse eu ipsum vulputate lectus dictum dictum eu id diam. 
+      Etiam vehicula sapien et lorem pharetra, id tincidunt lacus venenatis. 
+      Aliquam placerat cursus quam ac porttitor.
+    </p>
+    <p>
+      Nullam venenatis, erat et scelerisque pellentesque, elit purus fermentum mi, a mattis quam nibh molestie sapien. 
+      Fusce pretium justo et metus blandit porttitor. Praesent nec orci nec tortor porta semper ut eu enim. Nam ac nulla ex. 
+      Suspendisse mattis ac lorem id ultricies. Morbi eu neque placerat, rhoncus est vel, egestas arcu. 
+      Duis tempor elit sed magna bibendum, non pretium elit lobortis. 
+      Aenean efficitur mollis elit. Duis mattis enim ac massa vehicula, vel finibus nisi ultrices. 
+      Etiam vitae purus placerat, finibus tellus in, semper est. Morbi et consectetur orci. 
+      Nam eu tortor non erat fringilla pulvinar eu at quam. Suspendisse vestibulum pellentesque dui. 
+      Donec scelerisque, metus sed tempor vestibulum, ex erat porta est, sed ornare nisl lorem viverra lorem.
+    </p>
   </React.Fragment>
 )
